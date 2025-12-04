5076acb49c9b4d601d1640fb1f6b557d19b284a1
ayush patel <152726075+risingstar185@users.noreply.github.com>
2025-12-04 19:02:11 +0530

    Refactor email sending to use Brevo API

--- backend/services/Email.js
+++ backend/services/Email.js
@@ -1,30 +1,12 @@
-import nodemailer from "nodemailer";
 import dotenv from "dotenv";
-import { generateOTP } from "../utils/otpGenerator.js";
-
+import axios from "axios";
 
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  service: "gmail", // ✅ correct
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
-transporter.verify((error, success) => {
-  if (error) {
-    console.log("Gmail services failed", error);
-  } else {
-    console.log("Gmail configured properly and ready to send email");
-  }
-});
-
-  export const sendOTPEmail = async (email, otp) => {
+export const sendOTPEmail = async (email, otp) => {
   const html = `
     <div style="font-family: Arial, sans-serif; color: #333; line-height: 1.6;">
-      <h2 style="color: #075e54;">🔐 WhatsApp Web Verification</h2>
+      <h2 style="color: #075e54;">🔐FixMyCity Web Verification</h2>
       <p>Hi there,</p>
       <p>Your one-time password (OTP) to verify your WhatsApp Web account is:</p>
       <h1 style="background: #e0f7fa; color: #000; padding: 10px 20px; display: inline-block; border-radius: 5px; letter-spacing: 2px;">
@@ -32,17 +14,37 @@ transporter.verify((error, success) => {
       </h1>
       <p><strong>This OTP is valid for the next 5 minutes.</strong> Please do not share this code with anyone.</p>
       <p>If you didn’t request this OTP, please ignore this email.</p>
-      <p style="margin-top: 20px;">Thanks & Regards,<br/>WhatsApp Web Security Team</p>
+      <p style="margin-top: 20px;">Thanks & Regards,<br/>FixMyCity Web Security Team</p>
       <hr style="margin: 30px 0;" />
       <small style="color: #777;">This is an automated message. Please do not reply.</small>
     </div>
   `;
 
-  await transporter.sendMail({
-    from: `WhatsApp Web <${process.env.EMAIL_USER}>`,
-    to: email,
-    subject: "Your WhatsApp verification code",
-    html, // ✅ added html
-  });
-};
+  try {
+    const response = await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          name: "FIxMycity Web",
+           email: "patelayush9554@gmail.com" // Verified nahi chahiye — works without domain
+        },
+        to: [{ email }],
+        subject: "Your FIxMycity verification code",
+        htmlContent: html,
+      },
+      {
+        headers: {
+          "api-key": process.env.BREVO_API_KEY,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
+    console.log("OTP Email sent:", response.data);
+    return true;
+
+  } catch (error) {
+    console.error("Brevo Error:", error.response?.data || error);
+    return false;
+  }
+};
